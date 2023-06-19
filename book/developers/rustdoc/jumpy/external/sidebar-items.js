45a45872b12c77fa46f9447c2ccce678663945fc
lecoqjacob <lecoqjacob@gmail.com>
2023-06-19 00:48:48 +0000

    Deploying to gh-pages from @ lecoqjacob/jumpy@eefd8a3fd15034e69c0b3ee478bd17e24239ec19 🚀

---
+++ book/developers/rustdoc/jumpy/external/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":[["bevy",""],["bones_matchmaker_proto","Network protocol structs for the Jumpy matchmaker."],["egui","`egui`:  an easy-to-use GUI in pure Rust!"],["ggrs","GGRS"]]};
